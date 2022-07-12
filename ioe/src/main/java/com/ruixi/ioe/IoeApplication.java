a0e0b60dda1a62f560403d502c0f228847305feb
liang_peng <liangpeng316886@163.com>
2022-07-12 21:57:14 +0800

    初始化

---
+++ ioe/src/main/java/com/ruixi/ioe/IoeApplication.java
@@ -0,0 +1,16 @@
+package com.ruixi.ioe;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//扫描mapper
+@MapperScan("com.ruixi.ioe.mapper")
+public class IoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoeApplication.class, args);
+    }
+
+}
